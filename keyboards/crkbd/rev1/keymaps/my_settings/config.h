55e6c131253371008656105b9d9ca454db93b874
tomo <tomotaka.k612@gmail.com>
2023-10-11 22:48:32 +0900

    update

--- keyboards/crkbd/rev1/keymaps/my_settings/config.h
+++ keyboards/crkbd/rev1/keymaps/my_settings/config.h
@@ -1,8 +1,6 @@
 /*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,27 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
-/* Select hand configuration */
-
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
-
-#define USE_SERIAL_PD2
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_LED_COUNT 54
+#    define RGB_MATRIX_SPLIT \
+        { 27, 27 }
+#    define SPLIT_TRANSPORT_MIRROR
 #endif
-
-#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
--- keyboards/crkbd/rev1/keymaps/my_settings/rules.mk
+++ keyboards/crkbd/rev1/keymaps/my_settings/rules.mk
@@ -6,3 +6,5 @@ RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 # RGB_MATRIX_ENABLE = no
 OLED_ENABLE     = yes
 LTO_ENABLE      = yes
+
+# OLED_DRIVER_ENABLE = yes
